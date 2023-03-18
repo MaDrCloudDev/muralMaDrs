654b3e3010c48670150d1ba3d93fedca615e7ea7
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2023-03-18 11:02:15 -0700

    update cloudinary folder
    update cloudinary folder

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -10,7 +10,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
 	cloudinary,
 	params: {
-		folder: "YelpCamp",
+		folder: "muralMaDrs",
 		allowedFormats: ["jpeg", "png", "jpg"],
 	},
 });
